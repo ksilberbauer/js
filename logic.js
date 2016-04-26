281dc5a2bbd2fb6ad881c7a60460140a72c5691b
Kyle Silberbauer <kyle.silberbauer@outlook.com>
2016-04-25 22:55:28 -0400

    create todo item with delete button

--- logic.js
+++ logic.js
@@ -5,11 +5,23 @@ function addTodo(event) {
   var val = event.target.value.trim();
 
   if (event.which === 13 && val) {
-    var newTodo = document.createElement('li');
-    newTodo.innerHTML = val;
+    var newTodo = createTodoItem(val);
     todoList.insertBefore(newTodo, event.target);
     event.target.value = '';
   }
+}
+
+function createTodoItem(text) {
+    var newTodo = document.createElement('li');
+    var todoText = document.createTextNode(text);
+    var deleteButton = document.createElement('button');
+    var deleteText = document.createTextNode('X');
+
+    deleteButton.appendChild(deleteText);
+    newTodo.appendChild(todoText);
+    newTodo.appendChild(deleteButton);
+
+    return newTodo;
 }
 
 document.addEventListener('click', function(event) {
